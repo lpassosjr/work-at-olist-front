fd44a7d78973575f25e1fa49378fa2fbf0c1bf3a
Luiz Roberto <lpassosjr@gmail.com>
2019-05-26 01:53:41 -0300

    Refactoring functions and code style

--- src/prod/js/functions.js
+++ src/prod/js/functions.js
@@ -1,96 +1,180 @@
-var fullName = document.getElementById("fullName"),
-email = document.getElementById("email"),
-rePassword = document.getElementById("reentryPassword"),
-password = document.getElementById("password"),
-requiredBar = document.getElementById("requiredBar"),
-requiredList = document.getElementById("requiredList"),
-filledClass = "filled",
-errorClass = "error"
+function Init(){
 
-// validation Name
+    // Selectors
 
-fullName.onchange = function() { 
-    this.className = ""
-	if(this.value.length > 0){
-        this.className = filledClass;
-	}else{
-        this.className = errorClass
-	}
-}
+    var fullName = document.getElementById("fullName"),
+    email = document.getElementById("email"),
+    rePassword = document.getElementById("reentryPassword"),
+    password = document.getElementById("password"),
+    requiredBar = document.getElementById("requiredBar"),
+    requiredList = document.getElementById("requiredList"),
+    loginForm = document.getElementById("loginForm"),
+    loginBox = document.getElementById("loginBox"),
+    successBox = document.getElementById("successBox"),
+    loginButton = document.getElementById("loginButton"),
+    loadingButton = document.getElementById("loadingButton"),
+    formEventSuccess = new Event("formEvent.success"),
+    formEventError = new Event("formEvent.error"),
+    filledClass = "filled",
+    errorClass = "error";
 
-// validation Email
+    // Functions
 
-function validateEmail(email) {
-    var mail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return mail.test(String(email).toLowerCase());
-}
-
-email.onchange = function() {
-    this.className = ""
-    if (this.value.length > 0 && validateEmail(this.value)){
-        this.className = filledClass;
-    }else{
-		this.className = errorClass
-	}
-}
-
-// Validate Password 
-
-function validatePassword(password) {
-
-    if (password.value.length > 0){
- 
-        var rules = {
-            number: /\d/,
-            characters: /\w{6,6}/,
-            uppercase: /[A-Z]/,
-        };
-        for (var rule in rules) {
-            var regex = rules[rule]
-              document.getElementById(rule).className = (regex.test(password.value)
-            ? filledClass
-            : errorClass
-        )}
-
-        var bar = document.getElementsByClassName(filledClass).length;
-        if(bar === 1){
-            var bar = "oneCondition"
-            password.className = errorClass
-        }else if(bar === 2){
-            var bar = "twoCondition"
-            password.className = errorClass
-        }else if(bar === 3){
-            var bar = "threeCondition"
-            password.className = filledClass
-        }
-        requiredBar.className = bar;
-    }else{
-        requiredList.children[0].className = errorClass;
-        requiredList.children[1].className = errorClass;
-        requiredList.children[2].className = errorClass;
-        requiredBar.className = "";
-        password.className = errorClass;
-        rePassword.value = "";
-        rePassword.className = errorClass;
-
-    }
-}
-
-function confirmPassword(rePassword) {
-    if(rePassword.value.length !== 0){
-        if(rePassword.value === password.value){
-            rePassword.className = filledClass;
+    function validatefullName(fullName){
+        fullName.className = ""
+        if(fullName.value.length > 0 && !!fullName.value.trim()){
+            fullName.className = filledClass;
+            return true
         }else{
-            rePassword.className = errorClass;
+            fullName.className = ''
+            return false
         }
     }
-}
 
-password.onchange = function() {
-    validatePassword(this)
-    confirmPassword(rePassword)
-}
+    function validateEmailRegex(email) {
+        var mail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return mail.test(String(email).toLowerCase());
+    }
+    
+    function validateEmail(email){
+        email.className = ''
+        if (email.value.length > 0){ 
+            if(validateEmailRegex(email.value) && !!email.value.trim()){
+                email.className = filledClass;
+                return true
+            }else{
+                email.className = errorClass;
+            }
+        }else{
+            email.className = ''
+            return false
+        }
+    }
 
-rePassword.onchange = function() {
-    confirmPassword(this)
+    function validatePassword(password) {
+        password.className = ''
+        if (password.value.length > 0){ 
+            
+            if(!!password.value.trim()){
+        
+                var rules = {
+                    number: /\d/,
+                    characters: /\w{6,6}/,
+                    uppercase: /[A-Z]/,
+                };
+                for (var rule in rules) {
+                    var regex = rules[rule]
+                    document.getElementById(rule).className = (regex.test(password.value)
+                    ? filledClass
+                    : errorClass
+                )}
+                var filledItens = document.querySelector('#requiredList');    
+                var bar = filledItens.querySelectorAll('.filled').length;
+                if(bar === 1){
+                    var bar = "oneCondition"
+                    password.className = errorClass
+                }else if(bar === 2){
+                    var bar = "twoCondition"
+                    password.className = errorClass
+                }else if(bar === 3){
+                    var bar = "threeCondition"
+                    password.className = filledClass
+                }
+                requiredBar.className = bar;
+                return true
+            }else{
+                password.className = errorClass;
+                rePassword.value = '';
+            }
+        }else{
+            requiredList.children[0].className = '';
+            requiredList.children[1].className = '';
+            requiredList.children[2].className = '';
+            requiredBar.className = '';
+            return false
+        }
+    }
+
+    function confirmPassword(rePassword) {
+        if(rePassword.value.length !== 0 && !!rePassword.value.trim()){
+            if(rePassword.value === password.value){
+                rePassword.className = filledClass;
+                return true
+            }else{
+                rePassword.className = errorClass;
+                return false
+            }
+        }
+    }
+
+    function validateElements() {
+        if(
+            validateEmail(email) &&
+            validatefullName(fullName) &&
+            validatePassword(password) &&
+            confirmPassword(rePassword)
+        ){
+            document.dispatchEvent(formEventSuccess);
+            return true
+        }
+        document.dispatchEvent(formEventError);
+        return false
+    }
+
+    function sendForm(event){
+        event.preventDefault();
+        loginButton.style.display = "none";
+        loadingButton.style.display = "block";
+
+
+        var ajax = new XMLHttpRequest();
+        
+        ajax.open("POST", "https://python-olist-api.herokuapp.com/signup", true);
+        ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        
+        ajax.send("email=teste@teste.com");
+    
+        ajax.onreadystatechange = function() {
+            
+            if (ajax.readyState == 4 && ajax.status == 200) {
+                loginForm.style.display = "none";
+                successBox.style.display = "block";
+                loginBox.classList.add("successBox");
+            }
+        }
+    }
+
+    // Events
+    document.addEventListener("formEvent.success", function(){
+        document.getElementById("loginButton").removeAttribute("disabled")
+    }, false)
+    document.addEventListener("formEvent.error", function(){
+        document.getElementById("loginButton").setAttribute("disabled", true)
+    }, false)
+
+    loginForm.addEventListener("submit", function() {
+        sendForm(event)
+    });
+
+    password.addEventListener('keyup', function() {
+        validatePassword(this)
+        confirmPassword(rePassword)
+        validateElements()
+    })
+    rePassword.addEventListener('keyup', function(){
+        confirmPassword(this)
+        validateElements()
+    })
+
+    email.addEventListener('change', function(){
+        validateEmail(this)
+        validateElements()
+    })
+
+    fullName.addEventListener('change', function(){
+        validatefullName(this)
+        validateElements()
+    })
+    
 }
+Init();
